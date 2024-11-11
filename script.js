630fb1855c5f9419a6380343d9d92cdfa67e98d5
Lucas10203040 <110103447+Lucas10203040@users.noreply.github.com>
2024-11-11 09:10:59 -0300

    .

---
+++ script.js
@@ -0,0 +1,21 @@
+const caixaPrincipal = document.querySelector(".caixa__principal");
+const caixaPerguntas = document.querySelector(".caixa__perguntas");
+const caixaAlternativas = document.querySelector(".caixa__alternativas");
+const caixaResultados = document.querySelector(".caixa__resultados");
+const textoResultado = document.querySelector(".texto__resultado");
+
+caixa perguntas = [
+    {
+        enunciado: "pergunta 1"
+        alternativas: [
+            "alternativa 1";
+            "alternativa 1"
+        ]
+    }
+{
+    enunciado: "pergunta 2"
+    alternativas: [
+        "alternativa 1"
+        "alternativa 2"
+    ]
+}
